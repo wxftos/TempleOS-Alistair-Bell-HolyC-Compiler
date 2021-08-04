8231ae7354cb4acd1142e82fa5aed22a49fdd0b6
alistair <alistair@alistairbell.xyz>
2021-08-04 19:44:59 +0000

    Comment modes work
    Single + multiline are ignored by the parser

--- examples/comment.hc
+++ examples/comment.hc
@@ -1,1 +1,4 @@
-/* this is a comment */
+// this is a comment
+U8 non_comment = 'c';
+/* more comments */
+non_comment = 'a';
--- src/parser.c
+++ src/parser.c
@@ -52,21 +52,56 @@ parser_mode_characters(char *chars, const char current_char, enum parser_type *l
 	char start_char = *((char *)baton);
 	if (current_char == start_char) {
 		/* Change back to the default function. */
-		*next_call = parser_type_default;
+		*next_call = parser_mode_default;
 	};
 }
 
 void
-parser_type_default(char *chars, const char current_char, enum parser_type *last_type, struct parser_pinsor *pinsor, void *baton, void **next_call, struct parser_update_data *data)
+parser_mode_comment(char *chars, const char current_char, enum parser_type *last_type, struct parser_pinsor *pinsor, void *baton, void **next_call, struct parser_update_data *update_data)
+{
+    /* The decider char tells whether it is a single line or an multiline comment likewise to this one right here. */
+    char decider_char = *((char *)baton);
+    switch (current_char) {
+        /* Handle the case for single line comments. */
+        case '\n': {
+            /* End of single line comment. */
+            if (decider_char == '/') {
+                goto end_it;
+            }
+            return;
+        }
+        case '/': {
+            /* End of the multiline comment. */
+            if (chars[pinsor->right - 1] == '*') {
+                /* Newline plus a single line comment. */
+                goto end_it;
+            }
+            return;
+        }
+        default: {
+            return;
+        }
+    }
+    end_it: {
+        pinsor->left = pinsor->right;
+        ++pinsor->left;
+        /* Obvious lie :). */
+        *last_type = PARSE_TYPE_WHITESPACE;
+        *next_call = parser_mode_default;
+    }
+}
+
+void
+parser_mode_default(char *chars, const char current_char, enum parser_type *last_type, struct parser_pinsor *pinsor, void *baton, void **next_call, struct parser_update_data *data)
 {
 	switch (current_char) {
 		case '\t' ... ' ': {
-			if (*last_type != HOLYC_PARSE_TYPE_WHITESPACE) {
+			if (*last_type != PARSE_TYPE_WHITESPACE) {
 				parser_add_token(chars, pinsor, data);
 			}
 			pinsor->left = pinsor->right;
 			pinsor->left++;
-			*last_type = HOLYC_PARSE_TYPE_WHITESPACE;
+			*last_type = PARSE_TYPE_WHITESPACE;
 			break;
 		}
 
@@ -79,16 +114,26 @@ parser_type_default(char *chars, const char current_char, enum parser_type *last
 		case 0x5f ... 'z': {
 			
 			/* If a regular char proceeds a special add it to the list of tokens. */
-			if (*last_type == HOLYC_PARSE_TYPE_SPECIAL) {
+			if (*last_type == PARSE_TYPE_SPECIAL) {
 				parser_add_token(chars, pinsor, data);
 				pinsor->left = pinsor->right;
 			}
 
 			/* Set the last type to junk. */
-			*last_type = HOLYC_PARSE_TYPE_JUNK;
+			*last_type = PARSE_TYPE_JUNK;
 			break;
 		}
-
+        case '/': {
+            /* FALLTHROUGH */
+        }
+        case '*': {
+            if (pinsor->right != 0 && chars[pinsor->right - 1] == '/') {
+                *((char *)baton) = current_char;
+                *next_call = parser_mode_comment;
+                break;
+            }
+            goto special_default;
+        }
         case '"': {
             /* FALLTHROUGH */
         }
@@ -100,13 +145,14 @@ parser_type_default(char *chars, const char current_char, enum parser_type *last
 
 		/* Default is for special chars, easier to use fallthroughs for regular chars as they are more bunched up. */
 		default: {
+            special_default: {
+			    if (*last_type != PARSE_TYPE_WHITESPACE) {
+				    parser_add_token(chars, pinsor, data);
+			    }
+			    pinsor->left = pinsor->right;
 
-			if (*last_type != HOLYC_PARSE_TYPE_WHITESPACE) {
-				parser_add_token(chars, pinsor, data);
-			}
-			pinsor->left = pinsor->right;
-
-			*last_type = HOLYC_PARSE_TYPE_SPECIAL;
+			    *last_type = PARSE_TYPE_SPECIAL;
+            }
         }
 	}
 }
@@ -118,7 +164,7 @@ parser_chars(char *chars, uint32_t char_count, struct token **tokens, uint32_t *
 	struct parser_pinsor p = { 0 };
 
 	/* Start in default mode. */
-	parser_function callback = parser_type_default;
+	parser_function callback = parser_mode_default;
 
 	/* Always have a non incrimented buffer. */
 	char *chars_copy = chars;
@@ -132,7 +178,7 @@ parser_chars(char *chars, uint32_t char_count, struct token **tokens, uint32_t *
 	*tokens = (struct token *)calloc((char_count / 4) + 5, sizeof(*(*tokens)));
 
 	/* Last type of char that was inspected. */
-	enum parser_type last_type = HOLYC_PARSE_TYPE_WHITESPACE;
+	enum parser_type last_type = PARSE_TYPE_WHITESPACE;
 
 	/* Buffer to parse to the hasher where a token is stored. */
 	char *under_construction = (char *)malloc(HOLYC_UNDER_CONSTRUCTION_SIZE);
