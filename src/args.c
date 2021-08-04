7c45ebc9fbb006a426513de5c7b45f8aef3d7063
Alistair <alistair@alistairbell.xyz>
2021-08-04 13:25:24 +0100

    fucked up last commit

--- src/args.c
+++ src/args.c
@@ -4,11 +4,7 @@
 void
 arguments_help(void)
 {
-<<<<<<< HEAD
 	fprintf(stdout, "holyc: usage [-hvc:] [-h help] [-v version] [-c compile: file.hc]\n");
-=======
-	fprintf(stdout, "holyc: usage [-hv] [-h help] [-v version] file.hc\n");
->>>>>>> 9c08ed93174eaa496552e5195ee94aea10939317
 }
 void
 arguments_version(void)
@@ -22,25 +18,16 @@ int8_t
 arguments_handle(const int32_t argc, const char **argv, struct arguments_data *data)
 {
 	int opt;
-<<<<<<< HEAD
 	while ((opt = getopt(argc, (char *const *)argv, "hvc:")) != -1) {
-=======
-	while ((opt = getopt(argc, (char *const *)argv, "hv")) != -1) {
->>>>>>> 9c08ed93174eaa496552e5195ee94aea10939317
 		switch ((char)opt) {
 			case 'h': {
 				/* Display help. */	
 				arguments_help();
-<<<<<<< HEAD
 				return -1;
-=======
-				break;
->>>>>>> 9c08ed93174eaa496552e5195ee94aea10939317
 			}
 			case 'v': {
 				/* Display the build version. */
 				arguments_version();
-<<<<<<< HEAD
 				return -1;
 			}
 			case 'c': {
@@ -52,15 +39,4 @@ arguments_handle(const int32_t argc, const char **argv, struct arguments_data *d
 		}
 	}
 	return 0;
-=======
-				break;
-			}
-			default: {
-				data->compiling = argv[argc - 1];
-				return 1;
-			}
-		}
-	}
-	return -1;
->>>>>>> 9c08ed93174eaa496552e5195ee94aea10939317
 }
